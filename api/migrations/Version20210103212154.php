d54613e783be337f16a953f2dbee01d5f9a477d1
herveGuigoz <herve.guigoz@gmail.com>
2021-01-03 22:24:13 +0100

    ✨ feat: api entities

---
+++ api/migrations/Version20210103212154.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210103212154 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE transaction ADD customer_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D19395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_723705D19395C3F3 ON transaction (customer_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D19395C3F3');
+        $this->addSql('DROP INDEX IDX_723705D19395C3F3');
+        $this->addSql('ALTER TABLE transaction DROP customer_id');
+    }
+}
